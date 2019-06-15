16745c4d1812dc200af0dfa02bf235d23cd142ba
Cameron Moy <camoy@cs.umd.edu>
2019-06-15 12:48:09 -0400

    Add p2b

---
+++ p2b/src/higher.ml
@@ -0,0 +1,15 @@
+open Funs
+
+(********************************)
+(* Part 1: High Order Functions *)
+(********************************)
+
+let count_occ lst target = failwith "unimplemented"
+
+let uniq lst = failwith "unimplemented"
+
+let assoc_list lst = failwith "unimplemented"
+
+let flat_map f lst = failwith "unimplemented"
+
+let ap fns args = failwith "unimplemented"
